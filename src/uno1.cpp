62005c0f73ae121d6ebfc910d1c5035dfe0286bf
josuemorais <josuemorais@gmail.com>
2024-12-17 09:39:38 -0300

    refactor uno

--- src/uno1.cpp
+++ src/uno1.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
-
+#define PIN_LED 3
 float i = 0;
-void loop()
-{
+void escreve_serial(){
+    
   i += 0.1;
 
   // Print log
@@ -11,11 +11,32 @@ void loop()
 
   // Plot a sinus
   Serial.print(">sin:");
-  Serial.println(sin(i));
+  Serial.print(i);
+  Serial.print(":");
+  Serial.print(sin(i));
+  Serial.println("|g");
 
   // Plot a cosinus
   Serial.print(">Sum:");
   Serial.println(0.8 * sin(i) + 0.2 * cos(i));
 
   delay(50);
+}
+
+void blinkLed(uint8_t pin){
+  digitalWrite(pin,!digitalRead(pin));
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(PIN_LED, OUTPUT);
+}
+
+
+void loop()
+{
+  delay(500);
+  blinkLed(PIN_LED);
+  escreve_serial();
 }
